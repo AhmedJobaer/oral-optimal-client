0cf700b1a348504005d2074d6137c405dd2a62ad
jobaer-ahmed <ajobaer08@gmail.com>
2022-11-09 00:54:12 +0800

    service detail page added

--- src/Pages/Home/Services/ServiceDetail.js
+++ src/Pages/Home/Services/ServiceDetail.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const ServiceDetail = () => {
+    const { _id, title, price, img, description } = useLoaderData();
     return (
         <div>
-            <h2>Service Details</h2>
+            <div className="card card-compact bg-base-100 shadow-xl mb-4">
+                <figure><img className='' src={img} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{title}</h2>
+                    <p className='text-1xl text-orange-600 font-semibold'>Price: ${price}</p>
+                    <div className="card-actions justify-end">
+                        <p>{description}</p>
+                        <Link>
+                            <button className="btn btn-primary">Make Appoinment</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
