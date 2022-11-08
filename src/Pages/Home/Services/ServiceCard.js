58ebb75f3a8b4f63494c68719b54127e11a303be
jobaer-ahmed <ajobaer08@gmail.com>
2022-11-08 22:30:28 +0800

    load data from local mongodb server

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -3,16 +3,17 @@ import { Link } from 'react-router-dom';
 import '../Services/ServiceCard.css'
 
 const ServiceCard = ({ service }) => {
-    const { _id, img, price, title } = service;
+    const { _id, img, price, title, description } = service;
     return (
-        <div className="card card-compact bg-base-100 shadow-xl">
+        <div className="card card-compact bg-base-100 shadow-xl mb-4">
             <figure><img className='imgSize' src={img} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
-                <p className='text-2xl text-orange-600 font-semibold'>Price: ${price}</p>
+                <p className='text-1xl text-orange-600 font-semibold'>Price: ${price}</p>
                 <div className="card-actions justify-end">
+                    <p>{description}...</p>
                     <Link to={`/checkout/${_id}`}>
-                        <button className="btn btn-primary">Book Now</button>
+                        <button className="btn btn-primary">Read More</button>
                     </Link>
                 </div>
             </div>
