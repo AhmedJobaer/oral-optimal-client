45d215328778b3fa313afb376478fa3c28092a30
jobaer-ahmed <ajobaer08@gmail.com>
2022-11-10 20:05:25 +0800

    delete added

--- src/Pages/Review/MyReview.js
+++ src/Pages/Review/MyReview.js
@@ -15,6 +15,25 @@ const MyReview = () => {
     console.log(myReview);
 
 
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you Sure that you want to delete?');
+        if (proceed) {
+            fetch(`http://localhost:5000/myReview/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        alert('successfully deleted!');
+                        const remaining = myReview.filter(rm => rm._id !== id);
+                        setMyReview(remaining);
+                    }
+                })
+        }
+    }
+
+
     return (
         <div>
             <h2 className='text-center font-semibold text-2xl mb-4 text-orange-500'>ALL reviews for {user?.displayName}</h2>
@@ -30,6 +49,8 @@ const MyReview = () => {
                                         <p>Review: {r.comment}</p>
 
                                     </div>
+                                    <button onClick={() => handleDelete(r._id, r.service)} className="btn btn-outline btn-accent">Delete</button>
+                                    <button className="btn btn-outline  btn-warning">Update</button>
                                 </div>
                             </div>
                         </div>
